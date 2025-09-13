022bdc3df0bde48d1935a4e662f110ac383194d5
Dams2312 <225397863+Dams2312@users.noreply.github.com.>
2025-09-12 23:59:55 -0500

    chore: 🎉 Project start

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Nombre: Danny Velasco,  
+Fecha: 12/09/2025
+Descripción: Programa proveedor de  un API de tiquetes (boletos
+o tickets) para que cada vendedor de tiquetes lo use en su sistema.
+Necesitas proveer diferentes funcionalidades para ayudar a tu cliente a manejar el evento y
+vender sus tiquetes
+"""
