625c1713d4fcd055800e97728548866b7ad637d5
Brayan Snehyder Castro Velandia <222234032+01Brxyan@users.noreply.github.com>
2025-09-16 01:26:20 +0000

    feat: ✨ Agregar funcion de importacion

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-"""
-Nombre: Danny Velasco,  
-Fecha: 12/09/2025
-Descripción: Programa proveedor de  un API de tiquetes (boletos
-o tickets) para que cada vendedor de tiquetes lo use en su sistema.
-Necesitas proveer diferentes funcionalidades para ayudar a tu cliente a manejar el evento y
-vender sus tiquetes
-"""
+from modules.Tiquetes import menu_tiquetes
+
+if __name__ == "__main__":
+    menu_tiquetes()
